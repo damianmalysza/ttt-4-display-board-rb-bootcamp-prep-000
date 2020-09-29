72af8644e7d413e46971898de715b4e52841dbce
Damian Malysza <damian.malysza@gmail.com>
2020-09-29 00:02:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,3 +7,4 @@ def display_board(board_array)
   puts " #{board_array[3]} | #{board_array[4]} | #{board_array[5]} "
   puts "-----------"
   puts " #{board_array[6]} | #{board_array[7]} | #{board_array[8]} "
+end
